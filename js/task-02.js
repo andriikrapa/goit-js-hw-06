2068ccb64f712c81aa42a4c026294b41a60b3097
andriikrapa <andrii.krapa@gmail.com>
2022-02-08 12:43:13 +0100

    Finish task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('ul#ingredients');
+
+const ingredientsListEl = ingredients.map(ingredient => {
+  const listElement = document.createElement('li');
+  listElement.textContent = ingredient;
+  listElement.classList.add('item');
+
+  return listElement;
+});
+
+ingredientsEl.append(...ingredientsListEl);
