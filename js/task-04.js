5bceaa3b3acf828f2552f512c8568e2a888e5cb5
andriikrapa <andrii.krapa@gmail.com>
2022-02-09 13:05:11 +0100

    Done task-04 function in listener

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+
+const decrementBtnEl = document.querySelector('[data-action="decrement"]');
+const incrementBtnEl = document.querySelector('[data-action="increment"]');
+const counterValueEl = document.querySelector('#value');
+
+let counterValue = 0;
+
+decrementBtnEl.addEventListener("click", () => {
+    counterValue -= 1;
+    counterValueEl.textContent = counterValue;
+});
+
+incrementBtnEl.addEventListener("click", () => {
+    counterValue += 1;
+    counterValueEl.textContent = counterValue;
+});
